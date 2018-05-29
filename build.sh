22582065f527bfa0e535c694d20edb4e87503893
Eric Ross <eric.ross@hp.com>
2018-05-29 16:13:36 -0700

    Added ssl proxy

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-docker build -t pgbouncer --build-arg http_proxy="http://web-proxy.austin.hpicorp.net:8080" .
+docker build -t pgbouncer --build-arg http_proxy="http://web-proxy.austin.hpicorp.net:8080" --build-arg https_proxy="http://web-proxy.austin.hpicorp.net:8080" .
 
